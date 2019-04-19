78088c1b6f65e3d64fbf915f65705ff7c48b26bf
Alex <alexander.prutko@gmail.com>
2019-04-19 09:00:45 +0300

    train fix

--- torch_models.py
+++ torch_models.py
@@ -12,9 +12,9 @@ class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size()[0], -1)
 
-def flatten(t):
-    t = t.reshape(1, -1)
-    t = t.squeeze()
+def flatten(t, fdim):
+    t = t.reshape(fdim, -1)
+    # t = t.squeeze()
     return t      
 
 class STFT_CQT_Dataset(Dataset):
@@ -64,9 +64,9 @@ class STFT2CQT(nn.Module):
     def forward(self, input):
         return self.main(input)
 
-def gen_stft2cqt(layers):
+def gen_stft2cqt(layers, ngpu):
     class STFT2CQT_gen(nn.Module):
-        def __init__(self, ngpu):
+        def __init__(self, layers, ngpu):
             super(STFT2CQT_gen, self).__init__()
             self.ngpu = ngpu
             self.n_layers = len(layers)
@@ -74,14 +74,18 @@ def gen_stft2cqt(layers):
             self.bn = []
             for i in range(self.n_layers - 1):
                 self.linear.append(nn.Linear(layers[i], layers[i+1]))
+                self.add_module("Linear"+str(i), self.linear[-1])
             for i in range(self.n_layers - 2):
                 self.bn.append(nn.BatchNorm1d(layers[i+1]))
+                self.add_module("BatchNorm"+str(i), self.bn[-1])
 
         def forward(self, x):
             for i in range(self.n_layers - 2):
-                x = F.relu(self.bbn[i](self.linear[i](x)))
+                x = F.relu(self.bn[i](self.linear[i](x)))
             x = F.tanh(self.linear[-1](x))
             return x
+
+    return STFT2CQT_gen(layers, ngpu)
 
 class CQT2STFT(nn.Module):
     def __init__(self, ngpu):
@@ -115,7 +119,7 @@ class CQT2STFT(nn.Module):
 
 class CQT2STFT_conv(nn.Module):
     def __init__(self, ngpu):
-        super(CQT2STFT, self).__init__()
+        super(CQT2STFT_conv, self).__init__()
         self.ngpu = ngpu
 
         self.conv1 = nn.Conv1d(1, 16, 13, padding=6)
@@ -132,7 +136,7 @@ class CQT2STFT_conv(nn.Module):
         self.conv4 = nn.Conv1d(16, 32, 3, padding=1)
         self.bn4 = nn.BatchNorm1d(32)
 
-        self.linear5 = nn.Linear(1248, 2050)
+        self.linear5 = nn.Linear(2016, 2050)
         self.bn5 = nn.BatchNorm1d(2050)
 
         self.linear6 = nn.Linear(2050, 2050)
@@ -147,10 +151,10 @@ class CQT2STFT_conv(nn.Module):
         y = F.relu(self.bn3(self.conv3(input)))             # 108 x 1 -> 12 x 16
         y = F.relu(self.bn4(self.conv4(y)))                 # 12 x 16 -> 12 x 32
 
-        xf = flatten(x) # 864
-        yf = flatten(y) # 384
+        xf = flatten(x, input.shape[0]) # 864
+        yf = flatten(y, input.shape[0]) # 384
 
-        z = torch.cat((first_tensor, second_tensor))        # 1248
+        z = torch.cat((xf, yf), dim=1)        # 1248
 
         z = F.relu(self.bn5(self.linear5(z)))               # 2050
         z = F.relu(self.bn6(self.linear6(z)))               # 2050
